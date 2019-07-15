964f3788c8c3a80df55ffc5937ebb8102b37da6d
Yeonjeong Choi <makalu171@gmail.com>
2019-07-15 21:06:35 +0900

    express

--- main.js
+++ main.js
@@ -1,41 +1,50 @@
-var http = require('http');
-var url = require('url');
-var topic = require('./lib/topic');
-var author = require('./lib/author');
+const express = require('express');
+const app = express();
+const topic = require('./lib/topic');
+const author = require('./lib/author');
+const bodyParser = require('body-parser');
 
-var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            topic.home(response);
-        } else {
-            topic.page(request, response);
-        }
-    } else if (pathname === '/create') {
-        topic.create(request, response);
-    } else if (pathname === '/create_process') {
-        topic.create_process(request, response);
-    } else if (pathname === '/update') {
-        topic.update(request, response);
-    } else if (pathname === '/update_process') {
-        topic.update_process(request, response);
-    } else if (pathname === '/delete_process') {
-        topic.delete_process(request, response);
-    } else if (pathname === '/authors') {
-        author.home(request, response);
-    } else if (pathname === '/author/create_process') {
-        author.create_process(request, response);
-    } else if (pathname === '/author/update') {
-        author.update(request, response);
-    } else if (pathname ==='/author/update_process') {
-        author.update_process(request, response);
-    } else if (pathname ==='/author/delete_process') {
-        author.delete_process(request, response);
-    } else {
-        response.writeHead(404);
-        response.end('Not found');
-    }
+app.use(express.static('public'));
+app.use(express.static('css'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.get('/', (req, res) => topic.home(res));
+
+app.get('/page/:pageId', function(request, response){
+    topic.page(request, response);
 });
-app.listen(3000);
+
+app.get('/create', function(request, response) {
+    topic.create(request, response);
+});
+
+app.post('/create_process', function(request, response){
+    topic.create_process(request, response);
+});
+
+app.get('/update/:pageId', function(request, response) {
+    topic.update(request, response);
+});
+
+app.post('/update_process', function(request, response){
+    topic.update_process(request, response);
+});
+
+app.post('/delete_process', function(request, response){
+    topic.delete_process(request, response);
+});
+
+app.get('/authors', (request, response) => author.home(request, response));
+
+app.post('/author/create_process', (request, response) => author.create_process(request, response));
+
+app.get('/author/update/:authorId', (request, response) => author.update(request, response));
+
+app.post('/author/update_process', (request, response) => author.update_process(request, response));
+
+app.post('/author/delete_process', (request, response) => author.delete_process(request, response));
+
+app.use(function(req, res, next) {
+res.status(404).send('Sorry cant find that!');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
