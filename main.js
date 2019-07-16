6ab68387a7cdda8541eac82c6cb9612531fe702e
Yeonjeong Choi <makalu171@gmail.com>
2019-07-16 22:48:46 +0900

    준비..
    new topic을 위한 modal 추가

--- main.js
+++ main.js
@@ -2,27 +2,45 @@ const express = require('express');
 const app = express();
 const topic = require('./lib/topic');
 const author = require('./lib/author');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+    , busboy = require('then-busboy')
+    , fileUpload = require('express-fileupload');
+
 const topicRouter = require('./routes/topic');
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/author');
 var helmet = require('helmet');
 // app.use(helmet());
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
+
+
 app.use(express.static('public'));
 app.use(express.static('css'));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static('js'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// 기본 path를 /public으로 설정(css, javascript 등의 파일 사용을 위해)
+app.use(express.static(__dirname + '/public'));
+app.use(fileUpload());
 
 
-app.use('/', indexRouter);
-app.use('/topic', topicRouter);
+ app.use('/', indexRouter);
+// app.use('/topic', topicRouter);
 
-app.get('/authors', (request, response) => author.home(request, response));
-app.use('/author', authorRouter);
+// app.get('/authors', (request, response) => author.home(request, response));
+// app.use('/author', authorRouter);
 
 
-app.use(function(req, res, next) {
-res.status(404).send('Sorry cant find that!');
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry cant find that!');
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
+var db = require('../lib/db');
+
 var express = require('express');
 var router = express.Router();
 const topic = require('../lib/topic');
 
-router.get('/', (req, res) => topic.home(res));
+var query = `SELECT T.title as title 
+                  , A.name as name 
+               FROM topic AS T 
+              INNER JOIN author AS A 
+                 ON T.author_id = A.id`
 
+db.query(query, function (error, topics) {
+    router.get('/', (req, res) => res.render('index', {title: 'My-New-Human-Revolution', topics:topics} ));
+});
+
+//topic.home(res)
 module.exports = router;
