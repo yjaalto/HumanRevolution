09c7dbaba0b65711fe235db22593968052ec2009
Yeonjeong Choi <makalu171@gmail.com>
2019-08-03 00:52:52 +0900

    chapter 삭제/수정

--- routes/chapter.js
+++ routes/chapter.js
@@ -15,16 +15,16 @@ router.post('/create_process', function(request, response){
     chapter.create_process(request, response);
 });
 
-// router.get('/update/:pageId', function(request, response) {
-//     topic.update(request, response);
-// });
+router.get('/update/:topicId/:chapterId', function(request, response) {
+    chapter.update(request, response);
+});
 
-// router.post('/update_process', function(request, response){
-//     topic.update_process(request, response);
-// });
+router.post('/update_process', function(request, response){
+    chapter.update_process(request, response);
+});
 
-// router.post('/delete_process', function(request, response){
-//     topic.delete_process(request, response);
-// });
+router.post('/delete_process', function(request, response){
+    chapter.delete_process(request, response);
+});
 
 module.exports = router;
