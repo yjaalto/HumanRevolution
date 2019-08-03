2e6534ddd8ca278496282488447facc56379ad8b
Yeonjeong Choi <makalu171@gmail.com>
2019-08-03 11:44:28 +0900

    chapter views 추가
    조회수 카운트

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function (req, res) {
                       , IFNULL(T.description, '') as description
                       , A.name as name 
                       , (SELECT COUNT(*) FROM chapter WHERE topicid = t.id) as chaptercnt
+                      , (SELECT SUM(views) FROM chapter WHERE topicid = t.id) as views
                    FROM topic AS T 
                   INNER JOIN author AS A 
                      ON T.author_id = A.id`
